e338ec6daf9cd4fa0def442e741f8aacc5b31690
Hiayao <zhangyiy.123@qq.com>
2020-06-17 21:26:28 +0800

    请求封装好

---
+++ .history/src/http/api_20200617191317.js
@@ -0,0 +1,67 @@
+// 用来封装所有的请求
+import service from './index'
+
+export default {
+    // 获取首页数据(get)
+    recommend(){
+        console.log(res.data)
+        return service.get('/recommend')
+    },
+
+    // 搜索关键词(get)
+    search(){
+        return service.get('/search')
+    },
+
+    // 分类查询(get)
+    // 参数:id:默认分类的id
+    classification(id){
+        return service.get('/classification?mallSubId=${id}')
+    },
+
+    // 查询获取购物车数据(get)
+    getCard(){
+        return service.get('/getCard')
+    },
+
+    // 购物车加减商品(post)
+    // 参数:count: 购物车数量
+    //     id: 商品id
+    //     mallPrice: 价格
+    editCart(){
+        return service.post('/editCart',{
+            id,
+            mallPrice
+        })
+    },
+
+    // 分类查询(get)
+    classification(id){
+        return service.get('/classification?mallSubId=${id}')
+    },
+
+    // 分类查询(get)
+    classification(id){
+        return service.get('/classification?mallSubId=${id}')
+    },
+
+    // 分类查询(get)
+    classification(id){
+        return service.get('/classification?mallSubId=${id}')
+    },
+
+    // 分类查询(get)
+    classification(id){
+        return service.get('/classification?mallSubId=${id}')
+    },
+
+    // 分类查询(get)
+    classification(id){
+        return service.get('/classification?mallSubId=${id}')
+    },
+
+    // 分类查询(get)
+    classification(id){
+        return service.get('/classification?mallSubId=${id}')
+    },
+}
---
+++ .history/src/views/Home_20200617182649.vue
@@ -0,0 +1,41 @@
+<template>
+ <div>
+<div class="nn"></div>
+ </div>
+</template>
+
+<script>
+ export default {
+   name: '',
+   props: {
+   },
+   components: {
+
+   },
+   data () {
+     return {
+
+     }
+   },
+   methods: {
+
+   },
+   mounted() {
+
+   },
+   watch: {
+
+   },
+   computed: {
+
+   }
+ }
+</script>
+
+<style scoped lang='scss'>
+.nn{
+    width: 100px;
+    height: 100px;
+    background: skyblue;
+}
+</style>
